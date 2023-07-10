9a96e824c5b503272507929e5120a16f38d55678
LENOVO <lakshmi.sannithi@improve10x.com>
2023-07-10 16:44:40 +0530

    I Completed ExampleUnitTest,I passed test.

--- app/src/main/java/com/improve10x/currencyconversion/model/Currency.java
+++ app/src/main/java/com/improve10x/currencyconversion/model/Currency.java
@@ -4,4 +4,12 @@ import com.improve10x.currencyconversion.model.Data;
 
 public class Currency {
     private Data data;
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
 }
--- app/src/test/java/com/improve10x/currencyconversion/ExampleUnitTest.java
+++ app/src/test/java/com/improve10x/currencyconversion/ExampleUnitTest.java
@@ -4,6 +4,15 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.google.gson.Gson;
+import com.improve10x.currencyconversion.model.Currency;
+import com.improve10x.currencyconversion.network.CurrencyApi;
+import com.improve10x.currencyconversion.network.CurrencyServiceApi;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +22,14 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void getCurrencies() throws IOException {
+        CurrencyServiceApi currencyServiceApi = new CurrencyApi().createCurrencyServiceApi();
+        Call<Currency> call = currencyServiceApi.fetchCurrencies("wwJuHgd49InoL9jdjEcqlFEYcV9HmDqKB7TwjT6F");
+        Currency currency = call.execute().body();
+        assertNotNull(currency);
+        System.out.println(new Gson().toJson(currency));
     }
 }
