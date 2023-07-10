4bd75bca33f98c80a778b702619f7a04b5fd96ff
LENOVO <lakshmi.sannithi@improve10x.com>
2023-07-10 16:29:16 +0530

    I modified endpoint CurrencyServiceApi.java class

--- app/src/main/java/com/improve10x/currencyconversion/network/CurrencyServiceApi.java
+++ app/src/main/java/com/improve10x/currencyconversion/network/CurrencyServiceApi.java
@@ -1,12 +1,14 @@
 package com.improve10x.currencyconversion.network;
 
+import com.improve10x.currencyconversion.model.Currency;
 import com.improve10x.currencyconversion.model.Data;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface CurrencyServiceApi {
 
-    @GET("v1/latest?apikey=wwJuHgd49InoL9jdjEcqlFEYcV9HmDqKB7TwjT6F")
-    Call<Data>fetchCurrencies();
+    @GET("v1/latest")
+    Call<Currency>fetchCurrencies(@Query("apikey") String apikey);
 }
