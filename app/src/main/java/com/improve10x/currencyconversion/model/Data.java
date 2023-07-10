5821b24c1ec23b84310b3880d1cd41c61535f926
LENOVO <lakshmi.sannithi@improve10x.com>
2023-07-10 15:01:59 +0530

    I completed model class

---
+++ app/src/main/java/com/improve10x/currencyconversion/model/Data.java
@@ -0,0 +1,75 @@
+package com.improve10x.currencyconversion.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Data {
+    @SerializedName("AUD")
+    private Float australianDollar;
+    @SerializedName("BGN")
+    private Float bulgarianLev;
+    @SerializedName("BRL")
+    private Float brazilianReal;
+    @SerializedName("CAD")
+    private Float canadianDollar;
+    @SerializedName("CHF")
+    private Float swissFranc;
+    @SerializedName("CNY")
+    private Float chineseYuanRenminbi;
+    @SerializedName("CZK")
+    private Float czechKoruna;
+    @SerializedName("DKK")
+    private Float danishKrone;
+    @SerializedName("EUR")
+    private Float europeanUnion;
+    @SerializedName("GBP")
+    private Float britishPoundSterling;
+    @SerializedName("HKD")
+    private Float HongKongDollar;
+    @SerializedName("HRK")
+    private Float hrvatskaKuna;
+    @SerializedName("HUF")
+    private Float hungarianForint;
+    @SerializedName("IDR")
+    private Float indianRupee;
+    @SerializedName("ILS")
+    private Float israeliNewShekel;
+    @SerializedName("ISK")
+    private Float icelandicKrona;
+    @SerializedName("JPY")
+    private Float japaneseYen;
+    @SerializedName("KRW")
+    private Float koreanWon;
+    @SerializedName("MXN")
+    private Float mexicanPeso;
+    @SerializedName("MYR")
+    private Float malaysianRinggit;
+    @SerializedName("NOK")
+    private Float norwegianKrone;
+    @SerializedName("NZD")
+    private Float  newZealandDollar;
+    @SerializedName("PHP")
+    private Float philippinePeso;
+    @SerializedName("PLN")
+    private Float polishZloty;
+    @SerializedName("RON")
+    private Float romanianNewLeu;
+    @SerializedName("RUB")
+    private Float russianRuble;
+    @SerializedName("SEK")
+    private Float swedishKrona;
+    @SerializedName("SGD")
+    private Float singaporeDollar;
+    @SerializedName("THB")
+    private Float thaiBaht;
+    @SerializedName("TRY")
+    private Float turkishLira;
+    @SerializedName("USD")
+    private Float unitedStatesDollar;
+    @SerializedName("ZAR")
+    private Float SouthAfricanRand;
+
+
+
+
+
+}
