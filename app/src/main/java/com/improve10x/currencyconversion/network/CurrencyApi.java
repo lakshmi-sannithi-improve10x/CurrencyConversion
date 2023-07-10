021c3a4484431fb3b71bd9e60b7edf848a66d5d3
sunitha <sunithapraveenkumar@gmail.com>
2023-07-10 16:23:35 +0530

    createCurrencyServiceApi method is completed

--- app/src/main/java/com/improve10x/currencyconversion/network/CurrencyApi.java
+++ app/src/main/java/com/improve10x/currencyconversion/network/CurrencyApi.java
@@ -1,4 +1,23 @@
 package com.improve10x.currencyconversion.network;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class CurrencyApi {
+    public CurrencyServiceApi createCurrencyServiceApi(){
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.freecurrencyapi.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+        CurrencyServiceApi currencyServiceApi = retrofit.create(CurrencyServiceApi.class);
+        return currencyServiceApi;
+    }
 }
--- app/src/main/java/com/improve10x/currencyconversion/network/CurrencyServiceApi.java
+++ app/src/main/java/com/improve10x/currencyconversion/network/CurrencyServiceApi.java
@@ -1,4 +1,12 @@
 package com.improve10x.currencyconversion.network;
 
+import com.improve10x.currencyconversion.model.Data;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface CurrencyServiceApi {
+
+    @GET("v1/latest?apikey=wwJuHgd49InoL9jdjEcqlFEYcV9HmDqKB7TwjT6F")
+    Call<Data>fetchCurrencies();
 }
