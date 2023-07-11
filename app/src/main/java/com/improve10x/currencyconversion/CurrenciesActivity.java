346e6dba4a2167ff394ebee4c1e7f8c1637584c8
LENOVO <lakshmi.sannithi@improve10x.com>
2023-07-11 14:41:57 +0530

    I completed CurrenciesActivity coding part.

--- app/src/main/java/com/improve10x/currencyconversion/CurrenciesActivity.java
+++ app/src/main/java/com/improve10x/currencyconversion/CurrenciesActivity.java
@@ -3,16 +3,30 @@ package com.improve10x.currencyconversion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import com.improve10x.currencyconversion.databinding.ActivityCurrenciesBinding;
 
 public class CurrenciesActivity extends AppCompatActivity {
     private ActivityCurrenciesBinding binding;
+    private String[] currencies = {"australianDollar(AUD)","bulgarianLev(BGN)","brazilianReal(BRL)"};
+    private ArrayAdapter arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityCurrenciesBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setupAdapter();
+        connectAdapter();
 
+    }
+
+    private void connectAdapter() {
+        binding.currencySp.setAdapter(arrayAdapter);
+        binding.convertedCurrencySp.setAdapter(arrayAdapter);
+    }
+
+    private void setupAdapter() {
+        arrayAdapter = new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,currencies);
     }
 }
